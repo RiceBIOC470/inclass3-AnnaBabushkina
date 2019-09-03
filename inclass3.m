00059916d2638591d490e7fc9b30cf03c3df2275
warmflasha <warmflasha@gmail.com>
2019-09-03 11:36:20 -0500

    Add inclass3

---
+++ inclass3.m
@@ -0,0 +1,10 @@
+%1. Write code which prints a 10x10 magic square to a file. Then write code
+% that reads this file in and verifies that it is indeed a magic square.
+% Have this code print "true" if it is a magic square and "false" if it is
+% not. 
+
+%2. (a) Write code that makes a file with the words "Random numbers 1" on its
+%own line, then 10 lines with 13 random numbers each, then "Random numbers
+%2" on its own line, then 9 lines with 7 random numbers each. (b) Write code
+%that reads only the first line of random numbers in the file and stores
+%them as numbers in an array. 
